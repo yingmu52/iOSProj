7147415221cfe93cd876338506a3c9a1e36c3bf1
Xinyi Zhuang <yingmu52@msn.com>
2018-02-08 20:00:23 -0800

    Implement Layout of question view controller

---
+++ Connections/QuestionViewController.swift
@@ -0,0 +1,46 @@
+//
+//  QuestionViewController.swift
+//  Connections
+//
+//  Created by Xinyi Zhuang on 08/02/2018.
+//  Copyright © 2018 FBApps. All rights reserved.
+//
+
+import UIKit
+
+final class QuestionViewController: UIViewController {
+
+  @IBOutlet var imageView1: UIImageView!
+  @IBOutlet var imageView2: UIImageView!
+  @IBOutlet var imageView3: UIImageView!
+  @IBOutlet var imageView4: UIImageView!
+  @IBOutlet var questionLabel: UILabel!
+
+  @IBAction func buttonPressed(_ sender: UIButton) {
+    switch sender.tag {
+    case 0:
+      print("tag 0")
+    case 1:
+      print("tag 1")
+    case 2:
+      print("tag 2")
+    case 3:
+      print("tag 3")
+    default:
+      return
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // initialize view
+    questionLabel.text = nil
+    imageView1.image = nil
+    imageView2.image = nil
+    imageView3.image = nil
+    imageView4.image = nil
+
+    // TODO: Fetch and Display Data
+  }
+}
---
+++ Library/Storyboard.swift
@@ -0,0 +1,35 @@
+//
+//  Storyboard.swift
+//  Connections
+//
+//  Created by Xinyi Zhuang on 08/02/2018.
+//  Copyright © 2018 FBApps. All rights reserved.
+//
+
+import Foundation
+
+enum Storyboard: String {
+  case Main
+  case Question
+
+  func instantiate<T: UIViewController>(_ viewController: T.Type) -> T {
+    guard let vc = UIStoryboard(name: self.rawValue, bundle: nil)
+      .instantiateViewController(withIdentifier: T.storyboardID) as? T else {
+      fatalError("Couldn't instantiate \(T.storyboardID) from \(self.rawValue)")
+    }
+    return vc
+  }
+
+  var initialViewController: UIViewController {
+    guard let vc = UIStoryboard(name: self.rawValue, bundle: nil).instantiateInitialViewController() else {
+      fatalError("There should be at least one view controller in a storyboard file")
+    }
+    return vc
+  }
+}
+
+extension UIViewController {
+  static var storyboardID: String {
+    return description().components(separatedBy: ".").dropFirst().joined()
+  }
+}
